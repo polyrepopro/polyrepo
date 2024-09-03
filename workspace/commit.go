4bee6c2a9bc818f7120672d26c8106f8b7496be1
Matthew Davis <matthew@matthewdavis.io>
2024-09-02 23:47:33 -0500

    bump

--- workspace/commit.go
+++ workspace/commit.go
@@ -10,13 +10,15 @@ import (
 )
 
 func init() {
+	commitCommand.Flags().StringP("workspace", "w", "", "The name of the workspace to update.")
+	commitCommand.Flags().StringP("message", "m", "", "The message to commit with.")
 	WorkspaceCommand.AddCommand(commitCommand)
 }
 
 var commitCommand = &cobra.Command{
 	Use:   "commit",
-	Short: "commit the changes for each repository in the workspace",
-	Long:  "commit the changes for each repository in the workspace",
+	Short: "Commit the changes for each repository in the workspace.",
+	Long:  "Commit the changes for each repository in the workspace.",
 	Run: func(cmd *cobra.Command, args []string) {
 		setup, err := Setup("workspace.commit", util.GetArg[string](cmd, "workspace"), util.GetArg[string](cmd, "config"))
 		if err != nil {
